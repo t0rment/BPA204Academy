1ca70b315c9dc1942af98dfdd0bd82296c1c8443
mina <minahahame@gmail.com>
2026-03-04 18:34:41 +0400

    Add files via upload

---
+++ TASK.cs
@@ -0,0 +1,131 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+        //        // 1)task1
+        //        for (int i = 1; i <= 10; i++)
+        //            Console.WriteLine(i);
+
+        //        // 2) TASK2
+        //        int sum50 = 0;
+        //        for (int i = 1; i <= 50; i++)
+        //            sum50 += i;
+
+        //        Console.WriteLine("1-50 cəmi: " + sum50);
+
+        //        // 3) TASK3
+        //        for (int i = 10; i >= 1; i--)
+        //            Console.WriteLine(i);
+
+        //        // 4)TASK4
+        //        int evenSum = 0;
+        //        for (int i = 1; i <= 20; i++)
+        //        {
+        //            if (i % 2 == 0)
+        //                evenSum += i;
+        //        }
+        //        Console.WriteLine("1-20 cüt ədədlərin cəmi: " + evenSum);
+
+        //        // 5)TASK5
+        //        for (int i = 1; i <= 20; i++)
+        //        {
+        //            if (i % 2 != 0)
+        //                Console.WriteLine(i);
+        //        }
+
+        //        // 6)TASK6
+        //        string text = "Salam Dunya";
+        //        int length = 0;
+
+        //        for (int i = 0; i < text.Length; i++)
+        //            length++;
+
+        //        Console.WriteLine("Sətrin uzunluğu: " + length);
+
+        //        // 7)TASK
+        //        int sum100 = 0;
+        //        for (int i = 1; i <= 100; i++)
+        //            sum100 += i;
+
+        //        double average = sum100 / 100.0;
+        //        Console.WriteLine("1-100 ortalama: " + average);
+
+        //        // 8)TASK8
+        //        for (int i = 1; i <= 100; i++)
+        //        {
+        //            if (i % 5 == 0 && i % 7 == 0)
+        //                Console.WriteLine("5 və 7-yə bölünən: " + i);
+        //        }
+
+        //        // 9)TASK9
+        //        int evenTotal = 0;
+        //        int oddTotal = 0;
+
+        //        for (int i = 1; i <= 10; i++)
+        //        {
+        //            if (i % 2 == 0)
+        //                evenTotal += i;
+        //            else
+        //                oddTotal += i;
+        //        }
+
+        //        Console.WriteLine("Cütlərin cəmi: " + evenTotal);
+        //        Console.WriteLine("Təklərin cəmi: " + oddTotal);
+
+        //        // 10)TASK10
+        //        Console.WriteLine("Bal daxil edin:");
+        //        int score = int.Parse(Console.ReadLine());
+        //        Console.WriteLine("Qiymət: " + CheckGrade(score));
+
+        //        //ARRAY
+        //        //int[] arr = { 12, 14, 35, 56, 68, 93 };
+
+        //        //Console.WriteLine("Array cəmi: " + SumArray(arr));
+
+        //        //Console.WriteLine("Tək index-də duran elementlər:");
+        //        //for (int i = 0; i < arr.Length; i++)
+        //        //{
+        //        //    if (i % 2 != 0)
+        //        //        Console.WriteLine(arr[i]);
+        //        //}
+
+        //        //int evenIndexSum = 0;
+        //        //for (int i = 0; i < arr.Length; i++)
+        //        //{
+        //        //    if (i % 2 == 0)
+        //        //        evenIndexSum += arr[i];
+        //        }
+
+        //        Console.WriteLine("Cüt index cəmi: " + evenIndexSum);
+        //    }
+
+        //    // checkGrade 
+            
+        //    {
+        //        if (hesab >= 90)
+        //            return "A";
+        //        else if (hesab >= 80)
+        //            return "B";
+        //        else if (hesab >= 70)
+        //            return "C";
+        //        else
+        //            return "FAILED";
+        //    }
+
+        //    //SUM OF ALL ARRAY ELEMENTS
+        //    static int SumArray(int[] array)
+        //    {
+        //        int sum = 0;
+        //        for (int i = 0; i < array.Length; i++)
+        //            sum += array[i];
+
+        //        return sum;
+        //    }
+        //}
+    }
+    }
+}
